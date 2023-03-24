f9a72c5552e43dc34eac546c2ecc6b2810db1ec4
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-24 13:45:23 -0500

    onto java

---
+++ src/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 47;
+        float myNumber = 3.14159F;
+        String myString = "Chupa is the best boy.";
+
+        System.out.println(myFavoriteNumber);
+        System.out.println(myNumber);
+        System.out.println(myString);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        x=5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;
+        //int three = (int) "three";
+
+
+    }
+}
