1fb60a601501e2e315747dde489053b77bec4424
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-29 14:49:50 -0500

    finished shapes

--- src/InputTest.java
+++ src/InputTest.java
@@ -1,8 +1,11 @@
 import util.Input;
-
+import shapes.Circle;
 public class InputTest {
     public static void main(String[] args) {
         Input input = new Input();
+        Circle circle = new Circle(3);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
 
 //        int myInt = input.getInt(1,10);
 //        System.out.println(myInt);
@@ -10,7 +13,7 @@ public class InputTest {
 //        double myDouble = input.getDouble(1,100,"Yeller");
 //        System.out.println(myDouble);
 
-        String myString = input.getString();
-        System.out.println(myString);
+//        String myString = input.getString();
+//        System.out.println(myString);
     }
 }
