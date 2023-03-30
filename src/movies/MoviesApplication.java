de7458b3ca3cccbab7b670fb757e2df12ffd2b18
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-30 10:16:16 -0500

    movies array completed

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,62 @@
+package movies;
+import util.Input;
+
+import java.util.Arrays;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+        Input input = new Input();
+        boolean runProgram = true;
+
+        do {
+            showOptions();
+            int userInput = input.getInt(0,5);
+            String userOutput = getMovieGenre(userInput);
+
+            if(userOutput.equals("exit")){
+                runProgram = false;
+                System.out.println("Thanks for watching!");
+            } else {
+                setMovieCollection(userOutput);
+            }
+        } while (runProgram);
+    }
+
+    public static void showOptions(){
+        System.out.println("What would you like to do?");
+        System.out.println(" ");
+        System.out.println("0 - exit");
+        System.out.println("1 - view mall movies");
+        System.out.println("2 - view movies in animated category");
+        System.out.println("3 - view movies in drama category");
+        System.out.println("4 - view movies in horror category");
+        System.out.println("5 - view movies in scifi category");
+        System.out.println(" ");
+        System.out.println("Enter your choice: ");
+    }
+
+    public static void setMovieCollection(String input){
+        Movie [] movieCollection = new Movie[MoviesArray.findAll().length];
+
+        for(int i = 0; i < MoviesArray.findAll().length; i ++){
+            movieCollection[i] = MoviesArray.findAll()[i];
+
+            if(movieCollection[i].getCategory().equals(input)){
+                System.out.println(movieCollection[i].getName());
+            } else if (input.equals("all")) {
+                System.out.println(movieCollection[i].getName());
+            }
+        }
+    }
+
+    public static String getMovieGenre(int input){
+        return switch (input) {
+            case 1 -> "all";
+            case 2 -> "animated";
+            case 3 -> "drama";
+            case 4 -> "horror";
+            case 5 -> "scifi";
+            default -> "exit";
+        };
+    }
+}
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+    }
+}
